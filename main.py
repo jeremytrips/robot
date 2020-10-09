8a21e4c03471eb00bd68f9a87b6ce518dcafa35f
jeremytrips <51384000+jeremytrips@users.noreply.github.com>
2020-10-09 14:25:19 +0200

    Added main code

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import threading
 
 import settings
 
-from kivy.app import App
 from packages.robot import robot
-from emulator.RPi.GPIO import GPIO
-from emulator.interface.application import EmulatorApp
 
 if __name__ == "__main__":
     # todo parse arguments
-    app = EmulatorApp(robot.Robot, True)
+    
     try:
-        app.run()
+        a = Robot()
+        a.run()
     finally:
-        app.stop()
         GPIO.cleanup()
---
+++ main_debug.py
@@ -0,0 +1,20 @@
+import threading
+
+import settings
+
+from kivy.app import App
+from packages.robot import robot
+from emulator.RPi.GPIO import GPIO
+from emulator.interface.application import EmulatorApp
+
+if __name__ == "__main__":
+    # todo parse arguments
+    app = EmulatorApp(robot.Robot, True)
+    from packages.robot import robot
+    a = Robot()
+    a.run()
+    try:
+        app.run()
+    finally:
+        app.stop()
+        GPIO.cleanup()
